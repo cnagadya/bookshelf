c4df08335b538a1b17a83e45ccff39029b8544f5
Christine R. Nagadya <ncritah@gmail.com>
2020-10-10 10:19:08 +0200

    Exercise 06 - Extra 2: Error handling

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,21 @@
-import {loadDevTools} from './dev-tools/load'
+import { loadDevTools } from './dev-tools/load'
 import './bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './app'
+import { App } from './app'
+import { ReactQueryConfigProvider } from 'react-query'
+
+const queryConfig = {
+  queries: {
+    retry: (count, error) => error.status !== 404 && count < 2,
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+  },
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(<ReactQueryConfigProvider config={queryConfig}>
+    <App />
+  </ReactQueryConfigProvider>
+    , document.getElementById('root'))
 })
